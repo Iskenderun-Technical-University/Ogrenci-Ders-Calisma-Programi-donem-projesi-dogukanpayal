59563c2b5e386ad38692d068423e6b0dbb83e461
Dogukan Payal <payaldogukan@gmail.com>
2023-05-10 20:05:10 +0300

    front end kısmı bitti

--- Planner/calisma-ekrani/js/notes.js
+++ Planner/calisma-ekrani/js/notes.js
@@ -39,3 +39,58 @@ form.addEventListener('submit', (event) => {
     renderNotes();
   }
 });
+
+// up- down pop up kodları //
+
+function notEkleButtonPopUp()
+{
+  var notEkleFormu = document.getElementById("notes-container-form");
+  notEkleFormu.style.display = "flex";
+  var notesUpButtonFormu = document.getElementById("notes-up-button-form");
+  notesUpButtonFormu.style.display = "none";
+  var notesDownButtonFormu = document.getElementById("notes-down-button-form");
+  notesDownButtonFormu.style.display = "flex";
+}
+
+function notEkleButtonPopUpKapat()
+{
+  var notEkleFormu = document.getElementById("notes-container-form");
+  notEkleFormu.style.display = "none";
+  var notesUpButtonFormu = document.getElementById("notes-up-button-form");
+  notesUpButtonFormu.style.display = "flex";
+  var notesDownButtonFormu = document.getElementById("notes-down-button-form");
+  notesDownButtonFormu.style.display = "none";
+}
+
+
+function notesFormPopUp()
+{
+    var notEkleFormu = document.getElementById("notes-form");
+    notEkleFormu.style.display = "flex";
+    var notesUpButtonFormu = document.getElementById("notes-up-button-form");
+    notesUpButtonFormu.style.display = "none";
+    var notesDownButtonFormu = document.getElementById("notes-down-button-form");
+    notesDownButtonFormu.style.display = "flex";
+}
+
+function notesFormPopUpKapat()
+{
+    var notEkleFormu = document.getElementById("notes-form");
+    notEkleFormu.style.display = "none";
+    var notesUpButtonFormu = document.getElementById("notes-up-button-form");
+    notesUpButtonFormu.style.display = "flex";
+    var notesDownButtonFormu = document.getElementById("notes-down-button-form");
+    notesDownButtonFormu.style.display = "none";
+}
+
+function notesInputFormPopUp()
+{
+    var notesInputFormu = document.getElementById("notes-form");
+    notesInputFormu.style.display = "flex";
+}
+
+function notesInputFormPopUpKapat()
+{
+    var notesInputFormu = document.getElementById("notes-form");
+    notesInputFormu.style.display = "none";
+}
